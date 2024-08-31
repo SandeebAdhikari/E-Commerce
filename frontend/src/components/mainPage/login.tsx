00e27595118e3c80c3b4ffd468d35c824e4fa757
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-30 23:25:58 -0400

    add logo on login and signUp page

--- frontend/src/components/mainPage/login.tsx
+++ frontend/src/components/mainPage/login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Logo from "/images/sa-favicon-black.svg";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -20,7 +21,8 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
+      <img src={Logo} className="w-64" />
       <div className="w-1/4 h-1/2 flex flex-col items-center justify-center border shadow-sm shadow-slate-300 rounded-2xl">
         <div className="flex flex-col justify-center item-center w-3/4 gap-5">
           <h2 className="text-3xl font-bold text-center">Login</h2>
